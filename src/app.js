21afa6e46adad572decd4ca7d2c872891c03b517
Ian King <ian.antking@gmail.com>
2019-01-16 19:41:37 +0000

    changed to static for serving html

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
 const bookRouter = require('./routes/book');
-const path = require('path');
 
 const app = express();
+app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
 app.use('/book', bookRouter);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../index.html`));
+app.get('*', (_, res) => {
+  res.redirect('/');
 });
 
 module.exports = app;
