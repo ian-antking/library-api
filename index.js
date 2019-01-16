aa62a45793229af87ff8e625636ac69a7b46c93f
Ian King <ian.antking@gmail.com>
2019-01-16 19:26:53 +0000

    changed port to heroku environment variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const app = require('./src/app');
 
+const port = process.env.PORT || 4000;
+
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
-  app.listen(4000);
+  app.listen(port);
 });
