949a1e88812faeff79c287fae452d50cbeb373c3
Ian King <ian.antking@gmail.com>
2019-01-16 19:14:27 +0000

    app now serves static index.html page for documentation

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
 const bookRouter = require('./routes/book');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,9 @@ app.use(express.json());
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
 app.use('/book', bookRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/index.html`));
+});
 
 module.exports = app;
